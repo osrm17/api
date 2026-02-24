6cd4519ee5433c42a8b0d78c4f9eb880ba5922d5
Oscar Ramirez Gutierrez <rg.oscar17@ciencias.unam.mx>
2026-02-24 16:50:13 -0600

    Update clean up not used resources in file

--- app/main.py
+++ app/main.py
@@ -1,9 +1,4 @@
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-from typing import List
-from fastapi.params import Body
-from . import models, schemas, utils
-from .database import get_db
-from sqlalchemy.orm import Session
+from fastapi import FastAPI
 from .routers import post, user, auth
 
 app = FastAPI()
